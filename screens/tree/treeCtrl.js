34e1024139d8271a4dbfc787b4154e28e0b4b823
laurentperroteau <laurentperroteau@gmail.com>
2016-04-03 16:27:58 +0200

    SET tree html list

--- screens/tree/treeCtrl.js
+++ screens/tree/treeCtrl.js
@@ -2,17 +2,44 @@ let nElem
 
 const treeCtrl = () => {
 
+    const aTree =  [
+        {
+            isFile: false,
+            name: "components",
+            level1: [
+                {
+                    isFile: false,
+                    name: "debug",
+                    level2: [
+                        {
+                            isFile: true,
+                            "name": "debug.js"
+                        }
+                    ]
+                },
+                {
+                    isFile: false,
+                    name: "log",
+                    level2: [
+                        {
+                            isFile: true,
+                            "name": "log.js"
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            name: "app"
+        }
+    ]       
+
     const oTree = {
         "title": "Curriculum Vitae",
-        "menu": [
-            {
-                name: "app"
-            },
-            {
-                name: "components"
-            }
-        ]
+        "tree": aTree
     }        
+
+    console.log( oTree );
 
     const template = require('./tree.html')
 
