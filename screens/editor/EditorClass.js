b273d9d17ed24ef87de84963f83da0aa43726492
laurentperroteau <laurentperroteau@gmail.com>
2016-05-14 17:14:36 +0200

    SWITCH type highlight depend language

--- screens/editor/EditorClass.js
+++ screens/editor/EditorClass.js
@@ -10,7 +10,6 @@ import WriteClass from 'myComponents/write/WriteClass'
 class EditorClass extends CreateComponentClass {
 
     constructor( sName ) {
-
         super( sName )
 
         this.sName = sName
@@ -18,6 +17,11 @@ class EditorClass extends CreateComponentClass {
         this.nOutputCtn = null
     }
 
+    /**
+     * Init output (set elem and init class)
+     * =====================================
+     * @param  {string} sId => elem id
+     */
     initOutpupCtn( sId ) {
 
         this.nOutputCtn = document.getElementById( sId )
@@ -25,11 +29,22 @@ class EditorClass extends CreateComponentClass {
         this.WriteClass = new WriteClass( this.nOutputCtn, this.bIsTecnic )
     }
 
+    /**
+     * Init writting
+     * =============
+     * @param  {string} sCode => code to display
+     * @return {$promise}       
+     */
     initWrite( sCode ) {
 
         return this.WriteClass.init( sCode )
     }
 
+    /**
+     * Show output
+     * ===========
+     * @param  {string} sFileName 
+     */
     showOutput( sFileName ) {
 
         this._getRawText( sFileName )
@@ -46,31 +61,83 @@ class EditorClass extends CreateComponentClass {
         }, 100);
     }
 
-    // Array tabs method tested
-    // ------------------------
-    
+    /**
+     * Get raw text (async call)
+     * =========================
+     * @param  {string} sPathFile 
+     */
     _getRawText( sPathFile ) {
 
         debug( `GET ./tree/${sPathFile}.txt` );
 
+        const sLanguage = this._getLanguage( sPathFile )
+
         getAsync(`./tree/${sPathFile}.txt`).then( (data) => {
+
+            console.log( data );
                 
-            this._displayOutput( data )
+            this._displayOutput( data, sLanguage )
         })
     }
 
-    _displayOutput( sOutput ) {
+    /**
+     * Display/print code
+     * ==================
+     * @param  {string} sOutput   
+     * @param  {string} sLanguage => language-{js/css/html}
+     */
+    _displayOutput( sOutput, sLanguage ) {
 
+        // Stop writting
         if( this.WriteClass.isWritting ) {
-
             this.WriteClass.stop()
         }
 
+        // If convert HTML to string
+        if( sLanguage == 'language-html' ) {
+            sOutput = this._htmlEntities( sOutput )
+        }
+
+        // Print code
         this.nOutputCtn.innerHTML = sOutput
+
+        // Switch class for Prism
+        this.nOutputCtn.className = ''
+        this.nOutputCtn.classList.add( sLanguage )
 
         Prism.highlightAll()
 
         debug(`SHOW new fille, highlight then`)
+    }
+
+    /**
+     * Get current language
+     * ====================
+     * @param  {string} sPathFile 
+     * @return {string} => class name to add for Prism
+     */
+    _getLanguage( sPathFile ) {
+
+        if( sPathFile.indexOf('.html') !== - 1 ) {
+            return 'language-html';
+        }
+        else if( sPathFile.indexOf('.css') !== - 1 ) {
+            return 'language-css';
+        }
+        else {
+            return 'language-js';
+        }
+    }
+
+    /**
+     * HTML entities
+     * =============
+     * @param  {string} html markup
+     * @return {string} => html with escaped <, > and &
+     */
+    _htmlEntities( html ) {
+
+        return String(html).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;')
     }
 }
 export default EditorClass
