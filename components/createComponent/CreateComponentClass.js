90f5f264fef91bf24afc6619d1394d1f84ee535a
laurentperroteau <laurentperroteau@gmail.com>
2016-05-22 17:00:43 +0200

    USE camelCase lodash function

--- components/createComponent/CreateComponentClass.js
+++ components/createComponent/CreateComponentClass.js
@@ -1,5 +1,6 @@
 import debug from 'myComponents/debug/debug'
-import camelize from 'myComponents/debug/camelize'
+
+const _ = require('lodash') // TODO : require seulement besoin
 
 const rivets = require('rivets')
 
@@ -32,13 +33,14 @@ class CreateComponentClass {
         // Add CSS
         require( 'myScreens/'+ sPath + '.css' )
 
+        
         // Insert HTML
         if( this.nComponent === null ) {
 
             // It's a screens, and should be once.. then use an ID
-            this.nComponent = document.getElementById( camelize(`js ${this.sName}` ) )
-
-            if( html != '' ) {
+            this.nComponent = document.getElementById( _.camelCase(`js ${this.sName}`) )
+            
+            if( this.nComponent !== null && html != '' ) {
 
                 // Insert template
                 this.nComponent.innerHTML = html
@@ -47,6 +49,9 @@ class CreateComponentClass {
                 rivets.bind( this.nComponent, this.oData )
 
                 debug( `Init template: ${this.sName}` )
+            }
+            else {
+                console.warn( `The element ${this.sName} is null or empty` );
             }
         }
     }
