f52029d8bbaa2f3a65893c8020a5ff200016c92c
laurentperroteau <laurentperroteau@gmail.com>
2016-06-12 16:59:26 +0200

    MOVE app script

---
+++ src/app/component/app/app.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'my-app',
+    templateUrl: 'app.html',
+    styleUrls: ['app.css']
+})
+export class AppComponent {
+    fileName: "/home/laurentperroteau/www/cv/web/"
+    
+    /*
+    const App = new AppClass('app')
+
+    App.setData( oApp )
+
+    App.initTemplate()
+     */
+}
