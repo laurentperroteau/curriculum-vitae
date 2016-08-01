11dc33104a481971c4b3837fcdebe3bd3f072857
laurentperroteau <laurentperroteau@gmail.com>
2016-08-01 08:01:23 +0200

    INIT adaptation typescripit and angular2

--- src/app/component/app/app.ts
+++ src/app/component/app/app.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { TreeComponent } from './../tree/tree';
 
 @Component({
     moduleId: module.id,
     selector: 'my-app',
     templateUrl: 'app.html',
+    directives: [
+        TreeComponent,
+        // TabComponent,
+        // EditorComponent,
+    ],
     styleUrls: ['app.css']
 })
 export class AppComponent {
--- src/app/component/tree/tree.ts
+++ src/app/component/tree/tree.ts
@@ -1,7 +1,101 @@
-import TreeClass from 'myScreens/tree/TreeClass'
+import { Component } from '@angular/core';
 
-const treeCtrl = () => {
+@Component({
+    moduleId: module.id, // using by CommonJS, useful to set url relative to current folder
+    selector: 'my-tree',
+    templateUrl: 'tree.html',
+    styleUrls: ['tree.css']
+})
+export class TreeComponent {
+    title: String = 'Curriculum Vitae'
+    url: String   = './src/content/tree.json'
+    tree: any     = {}
 
+    constructor() {
+
+        this.getData()
+    }
+
+    getData() {
+
+        this.tree = {
+            "isFolder": true, 
+            "name": "", 
+            "children": [
+                {
+                    "fullPath": "./package.json", 
+                    "isFolder": false, 
+                    "name": "package.json"
+                }, 
+                {
+                    "fullPath": "./src", 
+                    "isFolder": true, 
+                    "name": "src", 
+                    "children": [
+                        {
+                            "fullPath": "./src/content", 
+                            "isFolder": true, 
+                            "name": "content", 
+                            "children": [
+                                {
+                                    "fullPath": "./src/content/demo.js", 
+                                    "isFolder": false, 
+                                    "name": "demo.js"
+                                }, 
+                                {
+                                    "fullPath": "./src/content/RESUME.md", 
+                                    "isFolder": false, 
+                                    "name": "RESUME.md"
+                                }, 
+                                {
+                                    "fullPath": "./src/content/tree.json", 
+                                    "isFolder": false, 
+                                    "name": "tree.json"
+                                }
+                            ]
+                        }
+                    ],
+                }
+            ]
+        }
+
+        console.log( this.tree )
+        // return $http( this.sUrl ).get()
+    }
+
+    triggerItem( oItem: any ) {
+
+        console.log( oItem )
+
+        if( oItem.isFolder ) {
+
+            this.triggerFolder( oItem )
+        }
+        else {
+            this.triggerFile( oItem )
+        }
+    }
+
+    triggerFolder( oItem: any ) {
+
+        console.log( oItem )
+
+        if( oItem.isOpen === undefined || !oItem.isOpen ) {
+            oItem.isOpen = true
+        }
+        else {
+            oItem.isOpen = false
+        }
+
+        // log( `Open Folder ${nElem.dataset.name}` )
+    }
+
+    triggerFile( oItem: any ) {
+
+        // TODO: si plusieurs folder on le même nom, il faudra ajouter une info
+        // PubSub.publish( 'OPEN_TAB', nElem.dataset )
+    }    
+    /*
     const Tree = new TreeClass('tree', './tree/tree.json')
 
     Tree.load().then( (data) => {
@@ -16,15 +110,13 @@ const treeCtrl = () => {
         })
             
         const oTree = {
-            "title": "Curriculum Vitae",
-            "tree": oResult
         } 
 
-        Tree.setData( oTree )
+        Tree.getData( oTree )
 
         Tree.initTemplate()
 
         Tree.setClickEvent()
     })
+     */
 }
-export default treeCtrl
