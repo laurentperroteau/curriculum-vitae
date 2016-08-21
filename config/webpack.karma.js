d98478ce8ae36b6414ad25857db2a2ff9c326174
laurentperroteau <laurentperroteau@gmail.com>
2016-08-21 16:06:12 +0200

    REMOVe chunks in webpack log

--- config/webpack.karma.js
+++ config/webpack.karma.js
@@ -7,25 +7,33 @@ const myWebpackResolve = require('./webpack.resolve.js')
 module.exports = function(config) {
     
     config.set({
+        basePath: '.',
         browsers: ['Chrome'],
         files: [
-            '../webpack.test.config.js',
+            '../webpack.test.config.js'
         ],
         frameworks: [
             'jasmine',
         ],
         preprocessors: {
             '../webpack.test.config.js': [
-                'webpack', 
-                'sourcemap'
+                'webpack'
             ]
         },
         reporters: ['spec'],
         webpack: {
             cache: true,
-            devtool: 'inline-source-map',
             module: myWebpackModule,
             resolve: myWebpackResolve,
-        }
+            devServer: {
+                stats: 'errors-only'
+            }
+        },
+        webpackMiddleware: {
+          stats: {
+            chunks: false,
+          }
+        },
+        //logLevel: config.LOG_DEBUG, //config.LOG_INFO,
     })
 }
