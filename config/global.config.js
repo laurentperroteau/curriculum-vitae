e9d863f964d0d088d123fad97262d4e114042068
laurentperroteau <laurentperroteau@gmail.com>
2017-02-27 21:52:46 +0100

    Init create-react-app with redux (remove conflicted files)

--- config/global.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    ENV                : 'developement',
-    PREVENT_TAB        : false,
-    LINKEDIN_PERFIL_URL: 'https://www.linkedin.com/in/laurent-perroteau-15a6ab68',
-    TITLE_TAG          : '<title>Laurent Perroteau | Développeur Front-End</title>',
-    DESCRIPTION_TAG    : '<meta name="description" content="Développeur Web depuis 5 ans, spécialisé Front-End depuis 4 ans, référent technique Front-End depuis 2 ans." />'
-}
