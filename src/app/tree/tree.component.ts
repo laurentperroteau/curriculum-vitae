5442e61de07f1daed9e0dbc50991b2cd8b901b59
laurentperroteau <laurentperroteau@gmail.com>
2017-05-01 10:49:30 +0200

    Add lodash and update tree.component

--- src/app/tree/tree.component.ts
+++ src/app/tree/tree.component.ts
@@ -1,36 +1,73 @@
-import {Component, ViewEncapsulation, OnInit} from '@angular/core';
-import {TreeService} from "../tree.service";
+import {
+  Component,
+  ViewEncapsulation,
+  OnInit
+} from '@angular/core';
+
+import * as _ from "lodash";
+
+import { TreeService } from "../tree.service";
 
 @Component({
-    selector: 'app-tree',
-    templateUrl: './tree.component.html',
-    // template: '{{tree.title}} {{tree.tree?.children}}<pre>{{ tree.tree?.children | json }}</pre>',
-    styleUrls: ['./tree.component.scss'],
-    encapsulation: ViewEncapsulation.None
+  selector: 'app-tree',
+  templateUrl: './tree.component.html',
+  styleUrls: ['./tree.component.scss'],
+  encapsulation: ViewEncapsulation.None // ??
 })
 export class TreeComponent implements OnInit {
     tree: any = {}
 
-    constructor(private treeService: TreeService) {
-        this.treeService = treeService
-    }
+  constructor(private treeService: TreeService) {
+      this.treeService = treeService
+  }
 
-    ngOnInit() {
+  ngOnInit() {
 
-        this.load().then( tree => {
+    this.load().then( (data: any) => {
 
-            this.tree = {
-                "title": "Curriculum Vitae",
-                "tree": tree
-            }
+      // Get all tree (include root)
+      // let oResult = JSON.parse( data )
 
-            console.log( this.tree )
-        })
-    }
+      console.log(data);
 
-    private load() {
+      // Sort subfolder
+      let oResult = _.forEach(data.children, function(oLevel1) {
 
-        return this.treeService.getTree()
-    }
+          if( oLevel1.isFolder ) {
+
+              oLevel1.children = _.sortBy(oLevel1.children, function(o: any) {
+                  return o.name.toLowerCase()
+              });
+
+              _.forEach(oLevel1.children, function(oLevel2: any) {
+
+                  oLevel2.children = _.sortBy(oLevel2.children, function(o: any) {
+                      return o.name.toLowerCase()
+                  });
+              });
+          }
+      });
+
+      // And sort first level
+      oResult = _.sortBy(oResult, function(o: any) {
+          return [ !o.isFolder, o.name.toLowerCase() ].join("_")
+      });
+
+      console.log(oResult);
+
+
+      this.tree = {
+        "title": "Curriculum Vitae",
+        "tree": {children: oResult}
+      }
+
+      console.log( this.tree );
+    })
+  }
+
+  private load() {
+
+    return this.treeService.getTree()
+  }
 
 }
